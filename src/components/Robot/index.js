d7f73add4073e4ce22e498f2cc4f4a7ac75eef72
Carlos Rosales <carlosros033@gmail.com>
2022-07-07 23:05:47 -0400

    feat: agregando logica de temporizadores

--- src/components/Robot/index.js
+++ src/components/Robot/index.js
@@ -1,22 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
+import { useEffect } from "react";
 import "./index.css";
 
 const Robot = ({ processIsActive }) => {
+  const [activeRun, setActiveRun] = useState(false);
+  const [drillIsActive, setDrillIsActive] = useState(false);
+
+  useEffect(() => {
+    if (!activeRun && processIsActive) {
+      setActiveRun(true);
+    }
+  }, [processIsActive, activeRun]);
+
+  useEffect(() => {
+    let firstDrillInterval;
+    let secondDrillInterval;
+    let activeRunInterval;
+
+    if (activeRun) {
+      console.log("intervals activated");
+      firstDrillInterval = setTimeout(() => {
+        if (activeRun) {
+          setDrillIsActive(true);
+        }
+      }, 2000);
+      secondDrillInterval = setTimeout(() => {
+        if (activeRun) {
+          setDrillIsActive(false);
+        }
+      }, 7000);
+      activeRunInterval = setTimeout(() => {
+        setActiveRun(false);
+      }, 10000);
+    } else {
+      setDrillIsActive(false);
+    }
+
+    return () => {
+      if (firstDrillInterval) {
+        clearTimeout(firstDrillInterval);
+      }
+      if (secondDrillInterval) {
+        clearTimeout(secondDrillInterval);
+      }
+      if (activeRunInterval) {
+        clearTimeout(activeRunInterval);
+      }
+    };
+  }, [activeRun]);
+
   return (
     <div className="robot-main-container">
       <div
-        className={
-          processIsActive ? "robot-conveyor-belt on" : "robot-conveyor-belt"
-        }
+        className={activeRun ? "robot-conveyor-belt on" : "robot-conveyor-belt"}
       ></div>
 
       <div
         className={processIsActive ? "robot-generator on" : "robot-generator"}
       ></div>
       <div
-        className={processIsActive ? "robot-final-box on" : "robot-final-box"}
+        className={activeRun ? "robot-final-box on" : "robot-final-box"}
       ></div>
-      <div className={processIsActive ? "robot-drill on" : "robot-drill"}></div>
+      <div className={drillIsActive ? "robot-drill on" : "robot-drill"}></div>
       <div className="robot-e1">
         <span className="robot-sensor-span">E1</span>
       </div>
@@ -29,9 +74,7 @@ const Robot = ({ processIsActive }) => {
       <div className="robot-t2">
         <span className="robot-sensor-span">T2</span>
       </div>
-      <div
-        className={processIsActive ? "robot-piece on" : "robot-piece"}
-      ></div>
+      <div className={activeRun ? "robot-piece on" : "robot-piece"}></div>
     </div>
   );
 };
