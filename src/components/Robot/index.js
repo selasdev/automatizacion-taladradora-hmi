8ec05af625517d791e0d2dfd452dbedb98bc5836
Carlos Rosales <carlosros033@gmail.com>
2022-07-07 22:35:21 -0400

    feat: adding sensors and drill to the robot

--- src/components/Robot/index.js
+++ src/components/Robot/index.js
@@ -9,12 +9,26 @@ const Robot = ({ processIsActive }) => {
           processIsActive ? "robot-conveyor-belt on" : "robot-conveyor-belt"
         }
       ></div>
+
       <div
         className={processIsActive ? "robot-generator on" : "robot-generator"}
       ></div>
       <div
         className={processIsActive ? "robot-final-box on" : "robot-final-box"}
       ></div>
+      <div className={processIsActive ? "robot-drill on" : "robot-drill"}></div>
+      <div className="robot-e1">
+        <span className="robot-sensor-span">E1</span>
+      </div>
+      <div className="robot-e2">
+        <span className="robot-sensor-span">E2</span>
+      </div>
+      <div className="robot-t1">
+        <span className="robot-sensor-span">T1</span>
+      </div>
+      <div className="robot-t2">
+        <span className="robot-sensor-span">T2</span>
+      </div>
     </div>
   );
 };
