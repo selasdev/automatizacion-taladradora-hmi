558d87ebe86efeb3088766ccc02ab4a54ce6a91f
Carlos Rosales <carlosros033@gmail.com>
2022-07-07 22:13:24 -0400

    feat: adding generator and final box to the robot

--- src/components/Robot/index.js
+++ src/components/Robot/index.js
@@ -2,7 +2,21 @@ import React from "react";
 import "./index.css";
 
 const Robot = ({ processIsActive }) => {
-  return <div>Robot</div>;
+  return (
+    <div className="robot-main-container">
+      <div
+        className={
+          processIsActive ? "robot-conveyor-belt on" : "robot-conveyor-belt"
+        }
+      ></div>
+      <div
+        className={processIsActive ? "robot-generator on" : "robot-generator"}
+      ></div>
+      <div
+        className={processIsActive ? "robot-final-box on" : "robot-final-box"}
+      ></div>
+    </div>
+  );
 };
 
 export default Robot;
